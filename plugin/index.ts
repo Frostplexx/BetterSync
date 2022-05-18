ec588b4c341379c6ae0391c3f85ae100ff836876
Frostplexx <daniel.inama02@gmail.com>
2022-05-18 19:41:59 +0200

    Use WebRTC

--- plugin/index.ts
+++ plugin/index.ts
@@ -1,5 +1,16 @@
 module.exports = (Plugin, Library) => {
 	const { Logger, Patcher, Settings, Toasts } = Library;
+	//import fs
+	const fs = require("fs");
+	let localConnection;
+	let remoteConnection;
+	let sendChannel;
+	let receiveChannel;
+	let fileReader;
+
+	let receivedSize = 0;
+
+	let bitrateMax = 0;
 
 	return class BetterSync extends Plugin {
 		constructor() {
@@ -9,74 +20,11 @@ module.exports = (Plugin, Library) => {
 			this.defaultSettings.option = 50;
 			this.defaultSettings.textbox = "";
 			this.defaultSettings.switch1 = false;
-			this.loggedIn = false;
-		}
-
-		async onStart() {
-			Logger.log("Started");
-			Patcher.before(Logger, "log", (t, a) => {
-				a[0] = "Patched Message: " + a[0];
-			});
-			//check login credentials
-			var username = this.settings.username;
-			var password = this.settings.password;
-			if (password == "" || username == "") {
-				setTimeout(function () {
-					Toasts.error("Please enter your Github credentials");
-				}, 1000);
-			} else {
-				//if success, set loggedIn to true
-				const xhr = await this.authenticate(username, password);
-				const status = xhr.status;
-				const message = xhr.responseText;
-				if (status === 200) {
-					this.loggedIn = true;
-				} else {
-					// else show error
-					Logger.log(message);
-					setTimeout(function () {
-						Toasts.error("GitHub authentication failed. Please check your credentials");
-					}, 1000);
-					this.loggedIn = false;
-				}
-			}
-			//if loggedIn is true, sync settings
-			console.log("Logged in: " + this.loggedIn);
-			if (this.loggedIn) {
-				setTimeout(function () {
-					Toasts.success("Successfully logged in");
-				}, 1000);
-				//check if github repo exists
-				this.checkRepo(username, password);
-			}
-		}
-
-		onStop() {
-			Logger.log("Stopped");
-			Patcher.unpatchAll();
 		}
 
 		getSettingsPanel() {
 			return Settings.SettingPanel.build(
 				this.saveSettings.bind(this),
-				new Settings.SettingGroup("Authentication", { shown: true }).append(
-					new Settings.Textbox(
-						"Github Username",
-						"Enter your Github Username",
-						this.settings.username,
-						(e) => {
-							this.settings.username = e;
-						}
-					),
-					new Settings.Textbox(
-						"Github Token",
-						"Enter your Github Personal Access Token",
-						this.settings.password,
-						(e) => {
-							this.settings.password = e;
-						}
-					)
-				),
 				new Settings.SettingGroup("Sync", { shown: true }).append(
 					new Settings.Switch(
 						"Sync",
@@ -90,96 +38,162 @@ module.exports = (Plugin, Library) => {
 			);
 		}
 
-		authenticate(username, password) {
-			return new Promise((resolve, reject) => {
-				Logger.log("Authenticating with Github");
-				var url = "https://api.github.com/user";
-				resolve(sendApiRequest(username, password, url, "GET", "basic", ""));
+		async onStart() {
+			Logger.log("Started");
+			Patcher.before(Logger, "log", (t, a) => {
+				a[0] = "Patched Message: " + a[0];
 			});
+
+			await this.createConnection();
 		}
 
-		checkRepo(username, password) {
-			//if repo exists, sync settings
-			//else create repo
-			var url = `https://api.github.com/user/repos`;
-			sendApiRequest(username, password, url, "GET", "token", "").then((response) => {
-				var found = false;
-				if (response.status === 200) {
-					const responseText = JSON.parse(response.responseText);
-					for(var i = 0; i < responseText.length; i++) {
-						if(responseText[i].name === "better-sync-plugin") {
-							console.log("Repo exists");
-							found = true;
-							break;
+		onStop() {
+			Logger.log("Stopped");
+			Patcher.unpatchAll();
+		}
+
+		async createConnection() {
+			localConnection = new RTCPeerConnection();
+			console.log("Created local peer connection object localConnection");
+
+			sendChannel = localConnection.createDataChannel("sendDataChannel");
+			sendChannel.binaryType = "arraybuffer";
+			console.log("Created send data channel");
+
+			sendChannel.addEventListener("open", this.onSendChannelStateChange);
+			sendChannel.addEventListener("close", this.onSendChannelStateChange);
+			sendChannel.addEventListener("error", this.onError);
+
+			localConnection.addEventListener("icecandidate", async (event) => {
+				console.log("Local ICE candidate: ", event.candidate);
+				await remoteConnection.addIceCandidate(event.candidate);
+			});
+
+			remoteConnection = new RTCPeerConnection();
+			console.log("Created remote peer connection object remoteConnection");
+
+			remoteConnection.addEventListener("icecandidate", async (event) => {
+				console.log("Remote ICE candidate: ", event.candidate);
+				await localConnection.addIceCandidate(event.candidate);
+			});
+			remoteConnection.addEventListener("datachannel", this.receiveChannelCallback);
+
+			try {
+				const offer = await localConnection.createOffer();
+				await this.gotLocalDescription(offer);
+			} catch (e) {
+				console.log("Failed to create session description: ", e);
+			}
+		}
+
+		onSendChannelStateChange() {
+			if (sendChannel) {
+				const { readyState } = sendChannel;
+				console.log(`Send channel state is: ${readyState}`);
+				if (readyState === "open") {
+
+
+					const file = fs.readFileSync(__dirname + "/test.txt");
+
+					console.log(`File is ${[file.name, file.size, file.type, file.lastModified].join(" ")}`);
+
+					// Handle 0 size files.
+					if (file.size === 0) {
+						this.closeDataChannels();
+						return;
+					}
+					const chunkSize = 16384;
+					fileReader = new FileReader();
+					let offset = 0;
+					fileReader.addEventListener("error", (error) => console.error("Error reading file:", error));
+					fileReader.addEventListener("abort", (event) => console.log("File reading aborted:", event));
+					fileReader.addEventListener("load", (e) => {
+						console.log("FileRead.onload ", e);
+						sendChannel.send(e.target.result);
+						offset += e.target.result.byteLength;
+						if (offset < file.size) {
+							readSlice(offset);
 						}
-					}
-					if(!found) {
-						console.log("Repo does not exist");
-						setTimeout(function () {
-							Toasts.warning("Repo does not exist. Creating repo...");
-						}, 1000);
-						this.createRepo(username, password);
-					} else {
-						this.syncSettings(username, password);
-					}
+					});
+					const readSlice = (o) => {
+						console.log("readSlice ", o);
+						const slice = file.slice(offset, o + chunkSize);
+						fileReader.readAsArrayBuffer(slice);
+					};
+					readSlice(0);
+
+
 				}
-			});
+			}
 		}
 
+		onError(error) {
+			if (sendChannel) {
+				console.error("Error in sendChannel:", error);
+				return;
+			}
+			console.log("Error in sendChannel which is already closed:", error);
+		}
 
-		createRepo(username, password) {
-			//create repo
-			var url = `https://api.github.com/user/repos`;
-			var data = {
-				name: "better-sync-plugin",
-				auto_init: true,
-				private: true
+		receiveChannelCallback(event) {
+			console.log("Receive Channel Callback");
+			receiveChannel = event.channel;
+			receiveChannel.binaryType = "arraybuffer";
+			receiveChannel.onmessage = this.onReceiveMessageCallback;
+			receiveChannel.onopen = this.onReceiveChannelStateChange;
+			receiveChannel.onclose = this.onReceiveChannelStateChange;
+
+			receivedSize = 0;
+			bitrateMax = 0;
+		}
+
+		async gotLocalDescription(desc) {
+			await localConnection.setLocalDescription(desc);
+			console.log(`Offer from localConnection\n ${desc.sdp}`);
+			await remoteConnection.setRemoteDescription(desc);
+			try {
+				const answer = await remoteConnection.createAnswer();
+				await this.gotRemoteDescription(answer);
+			} catch (e) {
+				console.log("Failed to create session description: ", e);
+			}
+		}
+
+		async gotRemoteDescription(desc) {
+			await remoteConnection.setLocalDescription(desc);
+			console.log(`Answer from remoteConnection\n ${desc.sdp}`);
+			await localConnection.setRemoteDescription(desc);
+		}
+
+		async sendData() {
+			const file = fs.readFileSync(__dirname + "/test.txt");
+
+			console.log(`File is ${[file.name, file.size, file.type, file.lastModified].join(" ")}`);
+
+			// Handle 0 size files.
+			if (file.size === 0) {
+				this.closeDataChannels();
+				return;
+			}
+			const chunkSize = 16384;
+			fileReader = new FileReader();
+			let offset = 0;
+			fileReader.addEventListener("error", (error) => console.error("Error reading file:", error));
+			fileReader.addEventListener("abort", (event) => console.log("File reading aborted:", event));
+			fileReader.addEventListener("load", (e) => {
+				console.log("FileRead.onload ", e);
+				sendChannel.send(e.target.result);
+				offset += e.target.result.byteLength;
+				if (offset < file.size) {
+					readSlice(offset);
+				}
+			});
+			const readSlice = (o) => {
+				console.log("readSlice ", o);
+				const slice = file.slice(offset, o + chunkSize);
+				fileReader.readAsArrayBuffer(slice);
 			};
-			sendApiRequest(username, password, url, "POST", "token", data).then((response) => {
-				console.log(response);
-				if(response.readyState == 4){
-					if (response.status === 201) {
-						console.log("Repo created");
-						this.syncSettings(username, password);
-					} else {
-						console.log("Repo creation failed");
-					}
-				}
-			});
+			readSlice(0);
 		}
-
-
-
-		syncSettings(username, password) {
-			//sync settings
-			console.log(__dirname)
-		}
-
 	};
-
-
-	function sendApiRequest(username, password, url, method, authtype, body) {
-		return new Promise((resolve, reject) => {
-			var xhr = new XMLHttpRequest();
-			xhr.open(method, url);
-			xhr.setRequestHeader("Accept", "application/vnd.github.v3+json");
-			if(authtype === "basic") {
-				xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));	
-			} else if(authtype === "token") {
-				xhr.setRequestHeader("Authorization", "token " + password);
-			}
-
-			if(body !== "") {
-				xhr.send(JSON.stringify(body));
-			} else {
-				xhr.send();
-			}
-			xhr.onreadystatechange = function () {
-				if (xhr.readyState == 4) {
-					resolve(xhr);
-				}
-			};
-		});
-	}
-
 };
